11039c467db9a65707c9072e0201d63fc973533f
Mike Morris <michael.patrick.morris@gmail.com>
2013-09-13 18:48:02 -0400

    rewrite

--- geojsonio.js
+++ geojsonio.js
@@ -4,6 +4,8 @@ var concat = require('concat-stream'),
     opener = require('opener'),
     tty = require('tty'),
     fs = require('fs'),
+    iniparser = require('iniparser'),
+    request = require('request'),
     argv = require('minimist')(process.argv.slice(2));
 
 if (argv.help || argv.h || !(argv._[0] || !tty.isatty(0))) return help();
@@ -11,20 +13,35 @@ if (argv.help || argv.h || !(argv._[0] || !tty.isatty(0))) return help();
 ((argv._[0] && fs.createReadStream(argv._[0])) || process.stdin).pipe(concat(openData));
 
 function openData(body) {
-    try {
-        var url = (argv.domain || 'http://localhost:3000/') +
-            '#data=data:application/json,' + encodeURIComponent(
-            JSON.stringify(JSON.parse(body.toString())));
+    var HOME = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 
-        if (process.stdout.isTTY) {
-            (argv.print ? console.log : opener)(url);
+    if (!process.stdout.isTTY) {
+        var gitconfig = iniparser.parseSync(HOME + '/.gitconfig');
+        var login = argv.login || (gitconfig.github && gitconfig.github.login);
+
+        if (login) {
+            request.post('https://api.github.com/authorizations', {
+                auth: {
+                    user: login
+                },
+                scopes: ['gist'],
+                note: 'geojsonio-cli'
+            }).pipe(process.stdout);
         } else {
-            process.stdout.write(body);
+            console.error('Valid --login argument required.');
         }
+    }
+
+    /*
+    try {
+        (argv.print ? console.log : opener)((argv.domain || 'http://geojson.io/') +
+            '#data=data:application/json,' + encodeURIComponent(
+            JSON.stringify(JSON.parse(body.toString()))));
     } catch(e) {
         console.error('Valid GeoJSON file required as input.');
         help();
     }
+    */
 }
 
 function help() { fs.createReadStream('README.md').pipe(process.stdout); }
